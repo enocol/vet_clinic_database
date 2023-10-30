a9c1a1224283b20ac486a564d43653892e186a59
Enoh Collins <eno_col@yahoo.com>
2023-10-30 15:46:27 +0000

    Update data.sql error

--- data.sql
+++ data.sql
@@ -1,10 +1,3 @@
 /* Populate database with sample data. */
 
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-3', 2, 'yes', 10.23 ) ;
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, 'yes', 8 ) ;
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, 'no', 15.04 ) ;
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimom', '2017-05-12', 5, 'yes', 11 ) ;
