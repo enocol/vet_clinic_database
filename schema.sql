1d8adf8928d0e6c375d0bd9e249892ba4d2731fe
Enoh Collins <eno_col@yahoo.com>
2023-10-30 14:37:48 +0000

    Update schema.sql for creating an animal table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+
+CREATE TABLE animals (id integer, name text, escape_attempts integer, neutered boolean, weight_kg decimal) ;
