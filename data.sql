006e23dbd4c034160b5e30d9727bf30f5005e705
Enoh Collins <eno_col@yahoo.com>
2023-10-30 22:18:50 +0000

    Update data.sql, escape_attempts for Agumon updated to 0

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-3', 2, 'yes', 10.23 ) ;
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-3', 0, 'yes', 10.23 ) ;
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, 'yes', 8 ) ;
 
