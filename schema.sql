803c820cc96d0d1000bfb0bb4168d71fce013067
Enoh Collins <eno_col@yahoo.com>
2023-11-03 11:21:16 +0000

    Update schema.sql. Create a new table and two join tables

--- schema.sql
+++ schema.sql
@@ -16,4 +16,10 @@ ALTER TABLE ANIMALS DROP COLUMN SPECIES;
 ALTER TABLE ANIMALS ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE ANIMALS ADD COLUMN owner_id INT REFERENCES owners(id);
 
+CREATE TABLE vet (id SERIAL PRIMARY KEY, name VARCHAR(255), age INT, date_of_graduation DATE);
+CREATE TABLE specializations (vet_id INT REFERENCES vet(id), species_id INT REFERENCES species(id), PRIMARY KEY (vet_id, species_id));
+CREATE TABLE visits (vet_id INT REFERENCES vet(id), animal_id INT REFERENCES animals(id), PRIMARY KEY (vet_id, animal_id));
+ALTER TABLE visits ADD COLUMN date_of_visit date;
+
+
 
